485d8e637bf614b89d976d7e16ae790bd2759836
debasish-itobuz <debasish.das@itobuz.com>
2024-11-12 15:35:57 +0530

    mongoose updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { BookModule } from './book/book.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { ConfigModule } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
+import { AppService } from "./app.service";
+import { BookModule } from "./book/book.module";
 
 @Module({
-  imports: [BookModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(
+      "mongodb://localhost:4002/book?retryWrites=true&w=majority&appName=Cluster0"
+    ),
+    BookModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
